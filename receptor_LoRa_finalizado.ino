1e246ccca71151446c5b1a167cf84d2bdba705fd
climasss <gabrielclimasp@gmail.com>
2025-08-10 14:53:24 -0300

    Update receptor_LoRa_finalizado.ino

--- receptor_LoRa_finalizado.ino
+++ receptor_LoRa_finalizado.ino
@@ -1,51 +1,51 @@
-#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
 
 // Configura os pinos para o módulo E220
-#define E220_RX 7
-#define E220_TX 8
+#define E220_RX 16    // RX do ESP32 (recebe do TX do E220)
+#define E220_TX 17    // TX do ESP32 (envia para RX do E220)
 #define PINO_GND 19
+#define PINO_GND2 18
 
-SoftwareSerial e220Serial(E220_RX, E220_TX); // RX, TX do E220
+HardwareSerial e220Serial(2); // Usando UART2 do ESP32
 
 void setup() {
-  Serial.begin(9600);       // Porta serial para monitoramento no PC
-  e220Serial.begin(9600);   // Porta serial para o E220
+  Serial.begin(9600);       
+  e220Serial.begin(9600, SERIAL_8N1, E220_RX, E220_TX); // Configura UART2 nos pinos definidos
+  
   pinMode(PINO_GND, OUTPUT);
-  digitalWrite(PINO_GND, LOW); // coloca o pino em 0V
+  digitalWrite(PINO_GND, LOW); 
+  pinMode(PINO_GND2, OUTPUT);
+  digitalWrite(PINO_GND2, LOW); 
+
   Serial.println("Iniciando E220...");
 }
 
 void loop() {
   if (e220Serial.available() > 0) {
-    uint8_t data[64]; // Buffer para os dados recebidos
+    uint8_t data[64]; 
     int len = 0;
 
-    // Lê todos os bytes disponíveis
     while (e220Serial.available() > 0) {
       data[len++] = e220Serial.read();
-      delay(10); // Aguarda pequenos atrasos entre bytes
+      delay(10); 
     }
 
-    if (len > 0 && data[0] == 'C' && data[1] == 'A' && data[2] == 'P' && data[3] == 'I'  ) {
-      // Exibe os dados recebidos
+    if (len > 0 && data[0] == 'C' && data[1] == 'A' && data[2] == 'P' && data[3] == 'I') {
       Serial.print("Dados recebidos: ");
-      for (int i = 6; i < len - 1; i++) { // Exclui o RSSI e a mensagem de codificação
+      for (int i = 6; i < len - 1; i++) { 
         Serial.print((char)data[i]);
       }
       Serial.println();
 
-      // Último byte é o RSSI
       uint8_t rssi = data[len - 1];
       Serial.print("RSSI (bruto): ");
       Serial.println(rssi);
 
-      // Opcional: Mapeia o RSSI para dBm (se a fórmula estiver disponível)
-      int rssi_dbm = -(256 - rssi); // Exemplo genérico, consulte o datasheet para ajustar
+      int rssi_dbm = -(256 - rssi);
       Serial.print("RSSI (em dBm): ");
       Serial.println(rssi_dbm);
     }
-
-    else{
+    else {
       Serial.print("Dados do capibaja não recebidos!!\n");
     }
   }
